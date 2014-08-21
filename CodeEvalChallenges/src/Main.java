6fb8794776c7bcfee633e1ace8ad1342c46594ae
wko <wko@NA192.wwf.local>
2014-08-21 13:24:49 +0200

    First Challenge (sum first thousand primes)

---
+++ CodeEvalChallenges/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+public class Main {
+	private static final int MAX_N = 1000;
+	public static void main(String[] args) {
+		int i = 2; 
+		ArrayList<Integer> primes = new ArrayList<>(MAX_N);
+		int sum = 0;
+		while (primes.size() < MAX_N) {
+			boolean isPrime = true;
+			for (int p : primes) {
+				if (i % p == 0) {
+					//p divides i
+					i += 1;
+					isPrime = false;
+					break;
+				}
+			}
+			if (isPrime) { 
+				primes.add(i);
+				sum += i;				
+			}
+			System.out.println(sum);
+		}
+	}
+
+}
